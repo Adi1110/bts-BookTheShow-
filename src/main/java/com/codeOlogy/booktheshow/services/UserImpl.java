a06e8d34e4c5ff183e8282501563838844c014a4
Aditya Ranjan <ranjan.aditya03@gmail.com>
2024-06-23 01:30:36 +0530

    implemented roles based authentication

---
+++ src/main/java/com/codeOlogy/booktheshow/services/UserImpl.java
@@ -0,0 +1,71 @@
+package com.codeOlogy.booktheshow.services;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.codeOlogy.booktheshow.entity.User;
+
+public class UserImpl implements UserDetails {
+
+    private String email;
+
+    private String password;
+
+    private Collection<? extends GrantedAuthority> roles;
+
+    public UserImpl(User user) {
+
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.roles = Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return this.roles;
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return this.password;
+    }
+
+}
