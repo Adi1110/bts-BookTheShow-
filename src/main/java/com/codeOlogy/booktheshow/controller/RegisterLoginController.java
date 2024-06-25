1fd8c874952bbdbd5bbe39e4e84e563e933c1984
Aditya Ranjan <ranjan.aditya03@gmail.com>
2024-06-25 23:44:08 +0530

    role based implements

--- src/main/java/com/codeOlogy/booktheshow/controller/RegisterLoginController.java
+++ src/main/java/com/codeOlogy/booktheshow/controller/RegisterLoginController.java
@@ -3,6 +3,7 @@ package com.codeOlogy.booktheshow.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -43,6 +44,7 @@ public class RegisterLoginController {
     private UserServiceImpl service;
 
     @RequestMapping("/welcome")
+    @PreAuthorize("hasRole('ADMIN')  or hasRole('USER')")
     public String welcome() {
 
         return "welcome to JWT";
--- src/main/java/com/codeOlogy/booktheshow/controller/UserController.java
+++ src/main/java/com/codeOlogy/booktheshow/controller/UserController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,28 +14,28 @@ import com.codeOlogy.booktheshow.services.UserServiceImpl;
 
 /**
  * @author Aditya Ranjan
- * Youtube : @Code_O_logy
- * Website : blogsnax.com
+ *         Youtube : @Code_O_logy
+ *         Website : blogsnax.com
  */
 
 @RestController
 @RequestMapping("/bts")
 public class UserController {
-    
+
     @Autowired
     private UserServiceImpl service;
-    
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/getUsers")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         System.out.println("getting users.......");
         return this.service.getUsers();
     }
 
-
     @GetMapping("/current-user")
+    @PreAuthorize("hasRole('USER')")
     public String getLoggedInUser(Principal principal) {
         return principal.getName();
     }
-    
 
 }
